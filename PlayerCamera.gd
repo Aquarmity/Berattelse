2c05baa6e40028a98848e9111d42917c2ec26d66
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2022-03-28 11:20:17 -0700

    Added camera

---
+++ PlayerCamera.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+onready var player = get_node("/root/Node2D/KinematicBody2D")
+
+
+func _ready():
+	set_limit_smoothing_enabled(true)
+	set_limit(0, 0)
+	set_limit(1, 0)
+	set_limit(2, 1024)
+	set_limit(3, 600)
+
+func _process (delta):
+	position.x = player.position.x
+	position.y = player.position.y
