b2e1d2772db9640772dfcfe5bd4238dcb32fc8c8
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2022-03-29 12:58:09 -0700

    Update player.gd
    Added the states, during the rolling state, velocity doesn't change

--- player.gd
+++ player.gd
@@ -4,9 +4,17 @@ export (int) var speed = 200
 
 var velocity = Vector2()
 
+enum state{idle, fighting, rolling, running, underAttack}
+var Time: float = 0.0000
+
+var player_state = state.idle
+
+
 func _ready():
 	var new_dialog = Dialogic.start('FirstConversation')
 	add_child(new_dialog)
+	get_input()
+
 
 func get_input():
 	velocity = Vector2()
@@ -20,6 +28,21 @@ func get_input():
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
-func _physics_process(delta):
+func _physics_process(delta: float):
 	get_input()
-	velocity = move_and_slide(velocity)
+	if velocity.x != 0 or velocity.y != 0:
+		player_state = state.running
+		velocity = move_and_slide(velocity)
+	elif Input.is_action_pressed("ui_accept") and (velocity.x != 0 or velocity.y != 0):
+		player_state = state.rolling
+		velocity = move_and_slide(velocity * 1.5)
+		_on_Timer_timeout()
+	else :
+		player_state = state.idle
+		velocity = move_and_slide(velocity)
+
+
+func _on_Timer_timeout():
+	velocity = velocity * 1.5
+	_physics_process(0.0)
+	
