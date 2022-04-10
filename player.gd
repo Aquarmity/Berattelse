0f40e16cb6474a86ebffa9db128285f3bda9c2cc
lucasericsson <lucasericsson222@gmail.com>
2022-04-10 15:35:09 -0700

    Movement Animation!

--- player.gd
+++ player.gd
@@ -7,8 +7,9 @@ var velocity = Vector2()
 enum state{idle, fighting, rolling, running, underAttack}
 var Time: float = 0.0
 
-var player_state = state.idle
+var player_state = state.running
 
+var last_direction = Vector2(0,0)
 
 
 func _ready():
@@ -31,7 +32,13 @@ func _physics_process(_delta):
 	if player_state == state.running:
 		# all the movement instructions for running
 		get_input()
+		if velocity != Vector2.ZERO:
+			last_direction = velocity
 		var running_velocity = velocity.normalized() * speed
+		if velocity != Vector2.ZERO:
+			set_run_anim(last_direction)
+		else:
+			set_stand_anim(last_direction)
 		move_and_slide(running_velocity)
 		
 		# any change state instructions
@@ -41,34 +48,35 @@ func _physics_process(_delta):
 			var timer = Timer.new()
 			timer.connect("timeout",self, "_on_Timer_timeout", [timer])
 			add_child(timer)
-			timer.wait_time = 0.1
-			timer.start()
-		if velocity == Vector2.ZERO:
-			player_state = state.idle
+			timer.start(0.2)
+
 	if player_state == state.rolling:
 		var fast_velocity = velocity * roll_speed
 		move_and_slide(fast_velocity)
-	#if player_state == state.attacking:
-		pass
-		# other places, to get into this state, should check if player can punch
-		# when punching, create a timer showing how long in this state
-		# create an area 2d that is the punching area in the direction the player is facing
-		# call the hurt() function on any enemies in that area
-		# if timer over, jump out
-	if player_state == state.underAttack:
-		pass
-			# create a hurt() function for the player
-		# have enemies call that function with their direction
-		# use that direction as new velocity = rebound sort of thing
-		# don't let controls move things
-		# use a timer to show how long in this state
-	
-	else: #any unknown state should just become idle
-		get_input()
-		if velocity != Vector2.ZERO:
-			player_state = state.running
+		
 func _on_Timer_timeout(args):
 	player_state = state.running
 	var timer_to_be_destroyed = args
 	timer_to_be_destroyed.queue_free()
+
+func set_run_anim(dir:Vector2):
+	if (dir.x > 0):
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
 	
+	if (dir.y >= 0):
+		$AnimatedSprite.play("rundown")
+	else:
+		$AnimatedSprite.play("runup")
+
+func set_stand_anim(dir:Vector2):
+	if (dir.x > 0):
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
+	
+	if (dir.y >= 0):
+		$AnimatedSprite.play("default")
+	else:
+		$AnimatedSprite.play("defaultup")
