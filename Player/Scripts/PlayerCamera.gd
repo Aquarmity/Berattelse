0b46c17e17e92f6aa8f37d007f97415edece797c
lucasericsson <lucasericsson222@gmail.com>
2022-04-16 20:59:36 -0700

    DON"T USE THIS ONE

--- Player/Scripts/PlayerCamera.gd
+++ Player/Scripts/PlayerCamera.gd
@@ -2,29 +2,15 @@ extends Camera2D
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
 
-export(Array, Vector2) var limit_points
 
 func _ready():
-	var bounds = get_tree().get_nodes_in_group("CameraBound")
+	#set_limit_smoothing_enabled(true)
+	#set_limit(0, 0)
+	#set_limit(1, 0)
+	#set_limit(2, 1024)
+	#set_limit(3, 600)
+	pass
 	
-	var minVector: Vector2 = bounds[0].position
-	var maxVector: Vector2 = bounds[0].position
-	
-	for bound in bounds:
-		bound = bound.position
-		if bound.x < minVector.x:
-			minVector.x = bound.x
-		if bound.y < minVector.y:
-			minVector.y = bound.y
-		if bound.x > maxVector.x:
-			maxVector.x = bound.x
-		if bound.y > maxVector.y:
-			maxVector.y = bound.y
-	
-	set_limit(MARGIN_LEFT, minVector.x)
-	set_limit(MARGIN_RIGHT, maxVector.x)
-	set_limit(MARGIN_BOTTOM, maxVector.y)
-	set_limit(MARGIN_TOP, minVector.y)
 func _process (_delta):
 	position.x = player.position.x
 	position.y = player.position.y
